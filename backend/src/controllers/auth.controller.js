afbaaeceafb3ab2be1641ffe2294f81522d16dfc
katyayani3102 <chauhankatyayani3102@gmail.com>
2025-08-29 16:48:12 +0530

    updated profilePic name in models

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -96,7 +96,7 @@ export const updateProfile = async (req, res) =>{
     const uploadResponse = await cloudinary.uploader.upload(profilePic)
     const updateUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true});
 
-    res.status(200).json({updateUser});
+    res.status(200).json(updateUser);
   } catch (error) {
     console.log("Error in update profilePic controller :", error.message);
     res.status(500).json({message: "Internal server error !!"});
--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         minlength: 6
     },
-    profilepic:{
+    profilePic:{
         type: String,
         default: "",
     },  
