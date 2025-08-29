1bcc61ee814c76aef7ed5a061fec9d7a9948f6b7
katyayani3102 <chauhankatyayani3102@gmail.com>
2025-08-29 09:42:26 +0530

    updated dir const var

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ import {app, server} from './lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT;
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 
 app.use(express.json());
